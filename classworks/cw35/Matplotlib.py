f5f1200326f370d0915a330168c1f52ef96ce372
Vardan <v.k.soghomonyan@gmail.com>
2021-12-11 22:37:31 +0400

    Added cw37 - Matplotlib options

--- classworks/cw35/Matplotlib.py
+++ classworks/cw35/Matplotlib.py
@@ -40,6 +40,16 @@ points = np.asarray([3, 1, 5, 4, 9, 1])
 points_x = np.asarray([2, 4, 3, 1, 7, 1])
 points1 = np.asarray([0, 9, 5, 6, 2, 5])
 points1_x = np.asarray([0, 6, 5, 2, 4, 1])
+
+# subplot divides plot area into provided subplots
+plt.subplot(2, 3, 1)
+plt.title("First graph")
+plt.plot(points)
+plt.subplot(2, 3, 2)
+plt.title("Second graph")
+plt.plot(points)
+plt.show()
+
 # if only one array is given, it is automatically considered as points of y axis. Points of x axis are integers
 plt.plot(points, marker='*')
 plt.plot(points)
@@ -60,6 +70,7 @@ plt.show()
 plt.plot(points, marker='o', markersize=20, mfc='#4CAF50', mec='red', linestyle='dotted', color='black', linewidth=5)
 plt.plot(points1, marker='*', markersize=15, mfc='#4CAF50', mec='blue', linestyle='dotted', color='purple',
          linewidth=10)
+plt.grid(axis='x')  # adding grid x axis on the plot
 plt.show()
 
 # plot 2 graphs on the same chart
@@ -68,5 +79,28 @@ font2 = {'family': 'serif', 'color': 'red', 'size': 15}
 plt.plot(points, points_x, points1, points1_x)
 plt.xlabel('Values', fontdict=font2)
 plt.ylabel('Points', fontdict=font2)
+plt.grid(color='green', linestyle='--', linewidth=0.5)  # adding grid on the plot
 plt.title('Average value of points', fontdict=font1)
+plt.show()
+
+colors = np.array(['red', 'black', 'blue', 'orange', 'purple', 'yellow'])
+colors1 = np.random.randint(100, size=6)
+# alpha is transparency
+plt.scatter(points, points1, color=colors, alpha=0.2)
+# cmap is color map - shade of colors
+plt.scatter(points_x, points1_x, c=colors1, cmap='Accent')
+plt.colorbar()
+plt.show()
+
+x = np.array(['A', 'B', 'C', 'D'])
+y = np.array([600, 400, 100, 300])
+plt.bar(x, y, width=0.5)
+plt.show()
+
+# for showing bar horizontally use barh
+plt.barh(x, y, color='red', height=0.2)
+plt.show()
+
+histo = np.random.normal(170, 10, 250)
+plt.hist(histo)
 plt.show()
